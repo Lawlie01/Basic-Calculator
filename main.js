58d733b31ea5dcd8403b82b5e07b49e90ebb2450
Lawlie <ahmetsadikoglu977@gmail.com>
2023-03-22 15:03:58 +0300

    esittir-duznlendi

--- main.js
+++ main.js
@@ -1,6 +1,76 @@
 // javascriptde bir kod çalıştırmak için öncelikle yazmamız gereken kod bloğu.
 
-window.onload = function(){
-    let numbers = document.querySelectorAll(".num")
-    
+window.onload = function () {
+
+    let selectedOperator = "";
+    let leftPart = "";
+    let rightPart = "";
+
+    let numbers = document.querySelectorAll(".num");
+    for (let i = 0; i < numbers.length; i++) {
+
+        numbers[i].addEventListener("click", function () {
+
+            //innerHTML - html içerisinden çek elementi demektir. 
+            document.getElementById("resultInner").innerHTML += this.value;
+
+            if (selectedOperator == "") {
+                leftPart += this.value;
+                // alert(leftPart);
+            }
+
+            else if (selectedOperator != "") {
+                rightPart += this.value;
+                // alert(rightPart);
+            }
+
+        }, false);
+    }
+
+
+    let operators = document.querySelectorAll(".operator");
+    for (let i = 0; i < operators.length; i++) {
+        operators[i].addEventListener("click", function () {
+            selectedOperator = this.value;
+
+            document.getElementById("resultInner").innerHTML += this.value;
+
+
+
+        }, false);
+    }
+    //  getelementByid - istediğimiz elementi çekmemizi sağlar. addEventListener("click", function()) ise tıklama efektini vermeyi sağlar.
+    document.getElementById("equation").addEventListener("click", function () {
+
+        if (selectedOperator == "+") {
+            document.getElementById("resultInner").innerHTML = leftPart + rightPart;
+        }
+
+        else if (selectedOperator == "-") {
+            document.getElementById("resultInner").innerHTML = leftPart - rightPart;
+
+        }
+
+        else if (selectedOperator == "*") {
+            document.getElementById("resultInner").innerHTML = leftPart * rightPart;
+
+        }
+
+        else if (selectedOperator == "/") {
+            document.getElementById("resultInner").innerHTML = leftPart / rightPart;
+
+        }
+
+        else if (selectedOperator == "%") {
+            document.getElementById("resultInner").innerHTML = leftPart % rightPart;
+
+        }
+
+
+    }, false);
+
+    document.getElementById("delete").addEventListener("click", function () {
+
+    }, false);
+
 };
